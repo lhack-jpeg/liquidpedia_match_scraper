a031b804026b59296de005e92257bf0ea9ab07d2
lhack-jpeg <luke-hackett@outlook.com>
2022-12-08 14:47:55 +1100

    add db_connect and field columns for match.

--- dota_match_scheduler/models.py
+++ dota_match_scheduler/models.py
@@ -1,8 +1,27 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, BigInteger, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from variables import DB
+
 
 Base = declarative_base()
 
 
+def db_connect():
+    """
+    Returns a connection instance.
+    """
+    return create_engine(f'mysql+mysqldb://{DB["USER"]}:{DB["PWORD"]}@localhost/{DB["DB"]}', pool_pre_ping=True)
+
+
 class Match(Base):
     __tablename__ = "matches"
+
+    id = Column(Integer, primary_key=True)
+    team_one = Column("team_one", String(128), nullable=False)
+    team_two = Column("team_two", String(128), nullable=False)
+    match_time = Column("match_time", DateTime, nullable=False)
+    epoch_time = Column("epoch", BigInteger, nullable=False)
+    team_one_id = relationship("Teams", backref="team_one_id")
+    team_two_id = relationship("Teams", backref="team_two_id")
+    # ! Need to add league_id to link back leagues table
