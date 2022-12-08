36fc469cd037d74cddaa763242b5da3ebc4a7b7f
lhack-jpeg <luke-hackett@outlook.com>
2022-12-08 14:49:37 +1100

    initialised save pipeline

--- dota_match_scheduler/pipelines.py
+++ dota_match_scheduler/pipelines.py
@@ -6,8 +6,31 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from models import db_connect, Match
+from sqlalchemy import sessionmaker
+from scrapy.exceptions import DropItem
 
 
 class DotaMatchSchedulerPipeline:
     def process_item(self, item, spider):
         return item
+
+
+class SaveMatchesPipeline(object):
+    def __init__(self):
+        """
+        Initialises cnnection.
+        """
+        engine = db_connect
+        self.Session = sessionmaker(bind=engine)
+
+    def process_item(self, item, spider):
+        """
+        Saves matches to the database. This is called on every item.
+        """
+        session = self.Session()
+        match = Match()
+        match.team_one = item["team_left"]
+        match.team_two = item["team_right"]
+        match.match_time = item["start_time"]
+        match.epoch_time = item["epoch_time"]
